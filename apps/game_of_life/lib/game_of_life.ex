b968cb4412586d50210d68fd532e32ba414209f4
TS Lim <tslim@flexnode.com>
2016-09-26 16:57:20 +0800

    Update game_of_life.ex

--- apps/game_of_life/lib/game_of_life.ex
+++ apps/game_of_life/lib/game_of_life.ex
@@ -2,20 +2,20 @@ defmodule GameOfLife do
   @moduledoc ~S"""
   Handles the logic for Conway's Game of Life.
 
-  Each cell is tracked a list that represents its coordinate
+  Each cell is tracked as a list that represents its coordinate
   E.g. [1, 2] = Column 1, Row 2
 
   On each iteration of the game, we gather a list of neighbours
   for all active cells. Then we apply the game of logic of each
   of them.
 
-  The state we pass to the next iteration is simply a list of
+  The state of the next iteration is simply a new list of
   active cells.
   """
 
   @doc ~S"""
   Transform current board state to the next iteration.
-  Returns a list of active cells
+  Returns a new list of active cells
   """
   def tick(state, board_size) do
     state
@@ -62,6 +62,9 @@ defmodule GameOfLife do
 
   @doc ~S"""
   Determines if a cell should be populated or not
+  - If a cell is currently populated and have 2-3 active neighbours, it stays alive
+  - If a cell is not currently populated but have 3 active neighbours, it will be populated
+  - In all other cases, the cell dies
   """
   def is_cell_populated?(cell, state, board_size) do
     active_neighbours = number_of_active_neighbours(cell, state, board_size)
@@ -76,7 +79,7 @@ defmodule GameOfLife do
   end
 
   @doc ~S"""
-  Calculate the number of active neighbours for a cell
+  Calculates the number of active neighbours for a cell
   """
   def number_of_active_neighbours(cell, state, board_size) do
     list_of_neighbours(cell, board_size)
